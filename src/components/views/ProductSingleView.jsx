00a166f3831c8ed6b0d06940d830d91876c96bed
filiperak <filip.erak@googlemail.com>
2024-03-05 19:03:43 +0100

    psv

--- src/components/views/ProductSingleView.jsx
+++ src/components/views/ProductSingleView.jsx
@@ -2,31 +2,47 @@ import { useContext, useEffect, useState } from "react";
 import { useParams, useNavigate, Navigate } from "react-router-dom";
 import instance from "../../services/api";
 import Loading from "../laoding/Loading";
+import Error from "../error/Error";
+
 
 const ProductSingleView = () => {
     const [product,setProduct] = useState(null);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+
     const navigate = useNavigate();
     const {productId} = useParams();
+
     console.log(productId);
     useEffect(() => {
-        instance.get(`/products/${productId}`).then((res) => setProduct(res.data))
-        .catch((error) => console.error("Error fetching", error));
-    },[])
+        instance.get(`/products/${productId}`)
+        .then((res) => {
+            setProduct(res.data)
+        })
+        .catch((er) => {
+            console.error("Error fetching", er)
+            setError(true);
 
+        })
+        .finally(() => {
+            setLoading(false);
+          });
+    },[])
     return (
-        <div>
-            singleView
-            {product? (
         <>
-            <img src={product.image} alt={product.title} />
-            <p>{product.title}</p>
-            <p>{product.description}</p>
-        </>
-        ) : (
-            <Loading/>
-        )}
+          {loading ? (
+            <Loading />
+          ) : error ? (
+            <Error error={error} />
+          ) : (
+            <div>
+                <img src={product.image} alt={product.title} />
+                <p>{product.title}</p>
+                <p>{product.description}</p>
             </div>
-        );
+          )}
+        </>
+      );
 }
  
 export default ProductSingleView;
