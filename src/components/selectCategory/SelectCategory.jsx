4dea5aead3534a4611e682b03a422aed6f08547e
filiperak <filip.erak@googlemail.com>
2024-03-09 21:02:40 +0100

    msg

--- src/components/selectCategory/SelectCategory.jsx
+++ src/components/selectCategory/SelectCategory.jsx
@@ -1,38 +1,4 @@
-// import axios from "axios";
-// import instance from "../../services/api";
-// import { useEffect, useState } from "react";
-// import ProductsList from "../products/ProductsList";
-// import { useContext } from "react";
-// import { ProductContext } from "../../contexts/ProductContext";
 
-// const SelectCaregory = () => {
-//     const {productState,productDispatch} = useContext(ProductContext);
-//     const {products} = productState;
-//     const [category,setCategory] = useState([])
-//     useEffect(() => {
-//         instance
-//         .get("/products/categories")
-//         .then((res) => setCategory(res.data))
-//         .catch((error) => {
-//           console.log(error);
-//         })
-//     },[category])
- 
-//     return (
-//         <div>
-//             <select name="select-category">
-//                 <option value={category}>all products</option>
-//                 {category.map(elem =>{
-//                 return <option value={elem} key={elem}>{elem}</option>
-//             })}
-//             </select>
-//             <ProductsList products={products} category={category}/>
-           
-//         </div>
-//     );
-// }
- 
-// export default SelectCaregory;
 import { useEffect, useState } from "react";
 import instance from "../../services/api";
 
