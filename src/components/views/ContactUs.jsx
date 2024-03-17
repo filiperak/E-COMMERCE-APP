12d077916bd43aa06db9bf2671124b424eff0dde
filiperak <filip.erak@googlemail.com>
2024-03-17 18:01:42 +0100

    toml

--- src/components/views/ContactUs.jsx
+++ src/components/views/ContactUs.jsx
@@ -1,24 +1,43 @@
+import { useState } from 'react';
 import './viewStyles/ContactUsStyle.css'
-
+import { useNavigate } from 'react-router-dom';
 
 const ContactUs = () => {
+    const navigate = useNavigate();
+    const [textAreaVal, setTextAreaVal] = useState('');
+
     const handleSubmit = (e) => {
-        e.preventDefault()
-    }
+        e.preventDefault();
+        setTextAreaVal('');
+    };
+
+    const handleTextAreaChange = (e) => {
+        setTextAreaVal(e.target.value);
+    };
 
     return (
         <div className='contact-container'>
-        <form className="contact-us-form" onSubmit={handleSubmit}>
-            <h2>Contact Us</h2>
-            <input type="email" id='inp-mail' placeholder='Enter Email'/>
-            <p>Message</p>
-            <textarea name="text-area" id="text-area" cols="30" rows="10"></textarea>
-            <p>
-            <input type="submit" value="Send" />
-            </p>
-        </form>
+            <div>
+                <span onClick={() => navigate(-1)}>Go Back</span>
+                <form className="contact-us-form" onSubmit={handleSubmit}>
+                    <h2>Contact Us</h2>
+                    <input type="email" id='inp-mail' placeholder='Enter Email'/>
+                    <p>Message</p>
+                    <textarea
+                        name="text-area"
+                        id="text-area"
+                        cols="30"
+                        rows="10"
+                        value={textAreaVal}
+                        onChange={handleTextAreaChange} 
+                    ></textarea>
+                    <p>
+                        <input type="submit" value="Send" id='contact-send-btn'/>
+                    </p>
+                </form>
+            </div>
         </div>
     );
-}
- 
+};
+
 export default ContactUs;
